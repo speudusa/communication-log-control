24f02d6f44396bb29990b0102f65aa6e288d290a
Courtney Frey <cfrey@launchcode.org>
2021-07-09 14:47:04 -0500

    my first commit

---
+++ comm-log-studio/comm-log-studio/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace comm_log_studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
